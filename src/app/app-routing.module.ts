865e052113f262288c77e585625542787804a8b1
Satish Hingu <Satish.Hingu@INFOVISIONLABS.COM>
2020-01-08 11:48:04 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: '',
+    redirectTo: '/header',
+    pathMatch: 'full'
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
